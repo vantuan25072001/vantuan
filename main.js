973f3682aa6ca6b1538133a0cf77b470362574b0
Nguyen Van Tuan <vantuan25072001@gamil.com>
2022-04-13 19:29:46 +0700

    Inital commit

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
-
-app.get('/tin', (req, res) => {
-    var a=1;
-    var b=2;
-    var c=a+b;
+app.use(morgan('combined'))
+app.get('/ti', (req, res) => {
+   
   res.send('Hello World!')
 })
 
